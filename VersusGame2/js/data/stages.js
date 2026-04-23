57b2342699ce7b8510e86f419fe23d26bb504e9a
Biju <iritachii@gmail.com>
2026-04-23 12:53:22 +0545

    updates

--- VersusGame2/js/data/stages.js
+++
@@ -1,8 +0,0 @@
-export const stages = [
-    { id: 1, name: "Aethelgard", background: "../assets/images/kingdoms/aethelgard.png" },
-    { id: 2, name: "Nyxara", background: "../assets/images/kingdoms/nyxara.png" },
-    { id: 3, name: "Flamma", background: "../assets/images/kingdoms/flamma.png" },
-    { id: 4, name: "Glaciar", background: "../assets/images/kingdoms/glaciar.png" },
-    { id: 5, name: "Mekhanika", background: "../assets/images/kingdoms/mekhanika.png" },
-    { id: 6, name: "The Wild Zones", background: "../assets/images/kingdoms/wildzones.png" }
-];
